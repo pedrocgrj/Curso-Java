3f451470407b95a8f1571988ac102e4cf5f6f7f6
pedrocgrj <32487543+pedrocgrj@users.noreply.github.com>
2018-09-04 12:25:32 -0300

    começando orientação

---
+++ teste/src/teste/Exer_6.java
@@ -0,0 +1,43 @@
+package teste;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer_6 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+/*		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter three student scores: ");
+		float nota_1 = sc.nextFloat();
+		float nota_2 = sc.nextFloat();
+		float nota_3 = sc.nextFloat();
+		float finalScores = (nota_1 + nota_2 + nota_3) / 3;
+		System.out.println("Score: " + nota_1 + " " + nota_2 + " " + nota_3);
+		System.out.printf("Final Score: %.1f%n", finalScores);
+		if (media < 6) {
+			System.out.println("Failed!");
+		}
+		sc.close();*/
+		
+
+/*		System.out.println("Enter glucose measure: ");
+		float gloucoseMeasure = sc.nextFloat();
+		
+		if(gloucoseMeasure <= 100) {
+			System.out.println("normal");
+		}else if(gloucoseMeasure <= 140) {
+			System.out.println("High");
+		}else {
+			System.out.println("Diabetes");
+		}*/
+		
+		
+		
+		sc.close();
+	}
+}
---
+++ teste/src/teste/POO.java
@@ -0,0 +1,10 @@
+package teste;
+
+public class POO {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
