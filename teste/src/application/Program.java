441acdd1111d74ab746afa1ab37892678f7f4c39
pedrocgrj <32487543+pedrocgrj@users.noreply.github.com>
2018-09-05 11:38:01 -0300

    Iniciando Membros Estáticos

--- teste/src/application/Program.java
+++ teste/src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Triangle;
-
 public class Program {
+	
+	public static final double PI = 3.14159;
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
@@ -37,7 +37,7 @@ public class Program {
 			System.out.println("Larger area: Y");
 		}*/
 		
-			Triangle x,y;
+			/*Triangle x,y;
 			
 			x = new Triangle();
 			y = new Triangle();
@@ -63,9 +63,25 @@ public class Program {
 				System.out.println("Larger area: X");
 			}else {
 				System.out.println("Larger area: Y");
-			}
+			}*/
+		
+			System.out.print("Enter radius: ");
+			double radius = sc.nextDouble();
+			
+			double c = circumference(radius);
+			double v = volume(radius);
+			
+			System.out.printf("Circumference: %.2f%n",c);
+			System.out.printf("Volume: %.2f%n",v);
+			System.out.printf("PI Value: %.2f%n",PI);
 		
 		sc.close();
 	}
-
+	
+	public static double circumference(double radius) {
+		return 2.0 * PI * radius;
+	}
+	public static double volume(double radius) {
+		return 4.0 * PI * radius * radius * radius / 3.0;
+	}
 }
